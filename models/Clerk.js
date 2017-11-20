431351b05ac0f60088785e8e93d9fbb7cbc5a08f
Ilialtes <jossue.perez@ucrso.info>
2017-11-20 09:34:49 -0600

    'cambios en el aplicant , el clerk model y el error ese del ojete del sendfile'

--- models/Clerk.js
+++ models/Clerk.js
@@ -1,6 +1,5 @@
-module.exports = function(ID, department, position) {
+module.exports = function(department, position) {
     //Validar con patrones
-    this.ID = ID;
     this.department = department;
     this.position = position;
 }
--- routes/applicants.js
+++ routes/applicants.js
@@ -32,103 +32,66 @@ router.get('/students', function(req, res, next) {
   });
 });
 
-// /*  
-// Create end update store procedur person definition
-// */
-//   function personStoreProcedure(req) {
-//     let applicant = req.body;
-//     //This is the applicant for the person object
-//     let person = new Person(applicant.identification, applicant.name, applicant.lastname,
-//                   applicant.email, applicant.tel, applicant.cel, applicant.expireDate,
-//                   applicant.districtID, applicant.signals, applicant.locationID);
-//     let initPool;
-//     //Send to database the person validating data
-//     DB_Connnection.then(pool => {
-//       initPool = pool; 
-//     });
-//     initPool.request()
-//     .input('identification',sql.NVarChar(50), person.ID)
-//     .input('name', sql.NVarChar(50),person.name)
-//     .input('lastname', sql.NVarChar(50), person.lastname)
-//     .input('email', sql.NVarChar(50), person.email)
-//     .input('tel', sql.NVarChar(15), person.tel)
-//     .input('cel', sql.NVarChar(15), person.cel)
-//     .input('expireDate', sql.DateTime, person.expireDate)
-//     .input('ID_district', sql.SmallInt, person.district)
-//     .input('signals', sql.NVarChar(50), person.signals)
-//     .input('locationID', sql.SmallInt, person.locationID);
-//     return initPool;
-// };
-
-
-//   /*
-//   Create and save a student
-//   */
-//   router.post('/student', function(req, res, next) {
-//     let student = req.body;
-//     let validatedStudent = new Student(student.studentID, student.career);
-//     let initPool = personStoreProcedure(req);
-//     DB_Connnection.then(pool => {
-//       return initPool.input('career', sql.NVarChar(50), validatedStudent.career)
-//       .input('studentID', sql.VarChar(10), validatedStudent.studentID)
-//       .output('ID', sql.Int)
-//       .execute('updateStudent');
-//     }).then(result => {
-//               res.send(result.output);
-//             }).catch(err => {
-//                 res.send('Fallo al ejecutar procedimiento.' + err);
-//             });
-//   });
-
-    /*
-  Create and save a student
-  */
-  router.post('/student', function(req, res, next) {
-    let student = req.body;
-    let validatedStudent = new Student(student.studentID, student.career);
+function popullatePersonPool(pool, req) {
     let applicant = req.body;
     //This is the applicant for the person object
     let person = new Person(applicant.identification, applicant.name, applicant.lastname,
                   applicant.email, applicant.tel, applicant.cel, applicant.expireDate,
                   applicant.districtID, applicant.signals, applicant.locationID);
-    //Send to database the person validating data
-    DB_Connnection.then(pool => {
-      return pool.request()
-        .input('identification',sql.NVarChar(50), person.identification)
+    pool.input('identification',sql.NVarChar(50), person.identification)
         .input('name', sql.NVarChar(50),person.name)
         .input('lastname', sql.NVarChar(50), person.lastname)
         .input('email', sql.NVarChar(50), person.email)
         .input('tel', sql.NVarChar(15), person.tel)
         .input('cel', sql.NVarChar(15), person.cel)
         .input('expireDate', sql.DateTime, person.expireDate)
-        .input('ID_district', sql.SmallInt, person.districtID)
+        .input('ID_district', sql.SmallInt, person.district)
         .input('signals', sql.NVarChar(50), person.signals)
-        .input('locationID', sql.SmallInt, person.locationID)
-        .input('career', sql.NVarChar(50), validatedStudent.career)
-        .input('studentID', sql.VarChar(10), validatedStudent.studentID)
-        .output('ID', sql.Int)
-        .execute('createStudent');
+        .input('locationID', sql.SmallInt, person.locationID);
+};
+
+
+  /*
+  Create and save a student
+  */
+  router.post('/student', function(req, res, next) {
+    let student = req.body;
+    let validatedStudent = new Student(student.studentID, student.career);
+    //Send to database the person validating data
+    DB_Connnection.then(pool => {
+       let poolRequest = pool.request();
+        popullatePersonPool(poolRequest, req);
+        return poolRequest.input('career', sql.NVarChar(50), validatedStudent.career)
+            .input('studentID', sql.VarChar(10), validatedStudent.studentID)
+            .output('ID', sql.Int)
+            .execute('createStudent');
       }).then(result => {
           res.send("User created: " + result.output);
       }).catch(err => {
           res.send('Fallo al ejecutar procedimiento.' + err);
      });
   });
+//this made the update of the student
+router.put('/student', function(req, res, next) {
+  let ID = req.body.ID;
+  let student = req.body;
+  let validatedStudent = new Student(student.studentID, student.career);
+  //Send to database the person validating data
+  DB_Connnection.then(pool => {
+     let poolRequest = pool.request();
+      popullatePersonPool(poolRequest, req);
+      return poolRequest.input('career', sql.NVarChar(50), validatedStudent.career)
+          .input('studentID', sql.VarChar(10), validatedStudent.studentID)
+          .input('ID', sql.Int, ID)
+          .execute('updateStudent');
+    }).then(result => {
+        res.send("User updated: " + ID);
+    }).catch(err => {
+        res.send('Fallo al ejecutar procedimiento.' + err);
+   });
+});
 
-// router.put('/student', function(req, res, next) {
-//   let ID = req.body.ID;
-//   let student = new Student(student.studentID, student.career);
-//   let request = personStoreProcedure(req);
-//   return request.input('career', sql.NVarChar(50), student.career)
-//           .input('studentID', sql.VarChar(10), student.studentID)
-//           .output('ID', sql.Int)
-//           .execute('createStudent').then(result => {
-//               res.send(result.output);
-//           }).catch(err => {
-//               res.send('Fallo al ejecutar procedimiento.' + err);
-//           });
-// });
-
+//delete an studen ENVIAR EL CARNET
 router.delete('/student/:id', function(req, res, next) {
     DB_Connnection.then(pool => {
       return pool.request()
@@ -157,38 +120,77 @@ router.get('/clerks', function(req, res, next) {
 /*
 Create and save a clerk
 */
-router.post('/clerk', function(req, res, next) {
-let clerk = req.body;
-let validateClerk = new Clerk(clerk.ID,clerk.department,clerk.position)   
-let applicant = req.body;
-//This is the applicant for the person object
-let person = new Person(applicant.identification, applicant.name, applicant.lastname,
-              applicant.email, applicant.tel, applicant.cel, applicant.expireDate,
-              applicant.districtID, applicant.signals, applicant.locationID); 
+// router.post('/clerk', function(req, res, next) {
+// let clerk = req.body;
+// let validateClerk = new Clerk(clerk.department,clerk.position)   
+// let applicant = req.body;
+// //This is the applicant for the person object
+// let person = new Person(applicant.identification, applicant.name, applicant.lastname,
+//               applicant.email, applicant.tel, applicant.cel, applicant.expireDate,
+//               applicant.districtID, applicant.signals, applicant.locationID); 
 
- DB_Connnection.then(pool => {
-      return pool.request()
-        .input('identification',sql.NVarChar(50), person.identification)
-        .input('name', sql.NVarChar(50),person.name)
-        .input('lastname', sql.NVarChar(50), person.lastname)
-        .input('email', sql.NVarChar(50), person.email)
-        .input('tel', sql.NVarChar(15), person.tel)
-        .input('cel', sql.NVarChar(15), person.cel)
-        .input('expireDate', sql.DateTime, person.expireDate)
-        .input('ID_district', sql.SmallInt, person.districtID)
-        .input('signals', sql.NVarChar(50), person.signals)
-        .input('locationID', sql.SmallInt, person.locationID)
-        .input('position', sql.NVarChar(50), validateClerk.career)
-        .input('department', sql.VarChar(50), validateClerk.studentID)
-        .output('ID', sql.Int)
-        .execute('createClerk');
+//  DB_Connnection.then(pool => {
+//       return pool.request()
+//         .input('identification',sql.NVarChar(50), person.identification)
+//         .input('name', sql.NVarChar(50),person.name)
+//         .input('lastname', sql.NVarChar(50), person.lastname)
+//         .input('email', sql.NVarChar(50), person.email)
+//         .input('tel', sql.NVarChar(15), person.tel)
+//         .input('cel', sql.NVarChar(15), person.cel)
+//         .input('expireDate', sql.DateTime, person.expireDate)
+//         .input('ID_district', sql.SmallInt, person.districtID)
+//         .input('signals', sql.NVarChar(50), person.signals)
+//         .input('locationID', sql.SmallInt, person.locationID)
+//         .input('department', sql.NVarChar(50), validateClerk.department)
+//         .input('position', sql.VarChar(50), validateClerk.position)
+//         .output('ID', sql.Int)
+//         .execute('createClerk');
+//       }).then(result => {
+//           res.send("User created: " + result.output);
+//       }).catch(err => {
+//           res.send('Fallo al ejecutar procedimiento.' + err);
+//      });
+//   });
+//create the new clerk 
+router.post('/clerk', function(req, res, next) {
+    let clerk = req.body;
+    let validateClerk = new Clerk(clerk.department, clerk.position);
+    //Send to database the person validating data
+    DB_Connnection.then(pool => {
+       let poolRequest = pool.request();
+        popullatePersonPool(poolRequest, req);
+        return poolRequest.input('department', sql.NVarChar(50), validateClerk.position)
+            .input('position', sql.VarChar(50), validateClerk.position)
+            .output('ID', sql.Int)
+            .execute('createClerk');
       }).then(result => {
-          res.send("User created: " + result.output);
+          res.send("clerk created: " + result.output);
       }).catch(err => {
           res.send('Fallo al ejecutar procedimiento.' + err);
      });
   });
+ //update the clerk 
 
+router.put('/clerk', function(req, res, next) {
+  let ID = req.body.ID;
+  let clerk = req.body;
+  let validateClerk = new Student(clerk.department, clerk.position);
+  //Send to database the person validating data
+  DB_Connnection.then(pool => {
+     let poolRequest = pool.request();
+      popullatePersonPool(poolRequest, req);
+      return poolRequest.input('department', sql.NVarChar(50), validateClerk.department)
+          .input('position', sql.VarChar(50), validateClerk.position)
+          .input('ID', sql.Int, ID)
+          .execute('updateClerk');
+    }).then(result => {
+        res.send("clerk updated: " + ID);
+    }).catch(err => {
+        res.send('Fallo al ejecutar procedimiento.' + err);
+   });
+});
+
+// delete clerk ENVIAR la cedula
   router.delete('/clerk/:id', function(req, res, next) {
     DB_Connnection.then(pool => {
       return pool.request()
@@ -200,4 +202,8 @@ let person = new Person(applicant.identification, applicant.name, applicant.last
         res.send("Clerk couldn't be deleted");
     });
 });
+
+
+
+
 module.exports = router;
