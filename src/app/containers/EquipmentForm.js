7bd5b9953de6577326fb8924b618a1034583e1f8
Steven <stiv-07@hotmail.com>
2017-11-23 00:41:13 -0600

    Equipment form refactor and Input form dinamyc fixed

--- src/app/containers/EquipmentForm.js
+++ src/app/containers/EquipmentForm.js
@@ -2,31 +2,61 @@ import React from "react";
 import {connect} from 'react-redux';
 
 import "../styles/Form.css";
-import {Input} from "../components/FormsUI/Input";
+import {Input, InputElement, SelectElement} from "../components/FormsUI/Input";
 
 class EquipmentForm extends React.Component {
 
-    render() {
+    constructor(props) {
+        super();
+        this.state = {
+            form: {
+                barcode : InputElement('text', 'Nombre', '', "barcode", "Codigo Barras"),
+                notes: InputElement('text', 'Notas', '', 'notes', "Notas"),
         
+                category : SelectElement( [
+                            {value:'1', displayValue: 'Laptop'},
+                            {value:'2', displayValue: 'Video Beam'}]
+                            ,'','category', "Categoria"),
+        
+                model : SelectElement( [
+                            {value:'1', displayValue: '12321'},
+                            {value:'2', displayValue: 'fsdfsd'}]
+                             ,'','model', "Modelo"),
+        
+                brand : SelectElement( [
+                           {value:'1', displayValue: 'VAIO'},
+                           {value:'2', displayValue: 'Apple'}]
+                            ,'','brand', "Marca"),
+        
+                state : SelectElement( [
+                            {value:'1', displayValue: 'Prestada'},
+                            {value:'2', displayValue: 'Disponible'}]
+                            ,'','state', "Estado")
+            }   
+        };
+    }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedform = {
+            ...this.state.form
+        }
+        const updatedFormElement = {
+            ...updatedform[inputIdentifier]
+        }
+        updatedFormElement.value = event.target.value;
+        updatedform[inputIdentifier] = updatedFormElement;
+        this.setState({form: updatedform});
+        console.log("Enrtree");
+    }
+
+    render() {
         const formElementsArray = [];
-        for (let key in this.props.equipment.form) { //Creates an array to loop through an object attributes
+        for (let key in this.state.form) { //Creates an array to loop through an object attributes
             formElementsArray.push({
                 id: key, //left side of attribute
-                config: this.props.equipment.form[key] //right side attribute
+                config: this.state.form[key] //right side attribute
             });
         }
-
-        // inputChangedHandler = (event, inputIdentifier) => {
-        //     const updatedform = {
-        //         ...this.props.equipment.form
-        //     }
-        //     const updatedFormElement = {
-        //         ...updatedform[inputIdentifier]
-        //     }
-        //     updatedFormElement.value = event.target.value;
-        //     updatedform[inputIdentifier] = updatedFormElement;
-        //     this.setState({form: updatedform});
-        // }
 
         return (
             <div className="formSpace">
@@ -40,18 +70,17 @@ class EquipmentForm extends React.Component {
                                     elementType={formElement.config.elementType}
                                     elementConfig={formElement.config.elementConfig}
                                     value={formElement.config.value}
-                                    // changed={(event) => this.inputChangedHandler(event, formElement.id)}
-                                    changed={(event) => this.props.equipment.onChangeValue(event.target.value, formElement.id)}
+                                    label={formElement.config.label}
+                                    changed={(event) => this.inputChangedHandler(event, formElement.id)}
                                 />
-                            ))}  
-                        </div>
-                            
-                        <div className="col-sm-2">
-                            
+                            ))}
+                                                        
+                        <div className="col-sm-4">
                             <button type="submit" className="btn btn-primary">{(this.props.function === 'CREATE')?"Crear":"Actualizar"}</button>
+                        </div>  
                         </div>
                     </form>
-            </div>
+                </div>
             </div>
 
         );
@@ -59,16 +88,18 @@ class EquipmentForm extends React.Component {
 
 }
 
-const mapStateToProps = (state) => {
-    return {
-        equipment: state.equipmentReducer
-    };
-};
+export default EquipmentForm;
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        onChangeValue: (value, inputIdentifier) => dispatch({type: "CHANGE_VALUE", payload: {value, inputIdentifier}})
-    };
-};
+// const mapStateToProps = (state) => {
+//     return {
+//         equipment: state.equipmentReducer
+//     };
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(EquipmentForm);
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         //onChangeValue: (value, inputIdentifier) => dispatch({type: "CHANGE_VALUE", payload: {value, inputIdentifier}})
+//     };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(EquipmentForm);
--- src/app/reducers/equipmentReducer.js
+++ src/app/reducers/equipmentReducer.js
@@ -1,55 +1,6 @@
 const initState = {
-    id_AV:'',
-    form: {
-        barcode : inputElement('text', 'Nombre', '', "barcode"),
-        notes: inputElement('text', 'Notas', '', 'notes'),
-
-        category : selectElement( [
-                    {value:'1', displayValue: 'Laptop'},
-                    {value:'2', displayValue: 'Video Beam'}]
-                    ,'','category'),
-
-        model : selectElement( [
-                    {value:'1', displayValue: '12321'},
-                    {value:'2', displayValue: 'fsdfsd'}]
-                     ,'','model'),
-
-        brand : selectElement( [
-                   {value:'1', displayValue: 'VAIO'},
-                   {value:'2', displayValue: 'Apple'}]
-                    ,'','brand'),
-
-        state : selectElement( [
-                    {value:'1', displayValue: 'Prestada'},
-                    {value:'2', displayValue: 'Disponible'}]
-                    ,'','state')
-    }
+    id_AV:''
 };
-
-function inputElement( type, placeholder, value, name) {
-    return {
-        elementType: 'input',
-        elementConfig: {
-            type: type,
-            placeholder: placeholder,
-            name: name,
-            id: name
-        },
-        value:value
-    };
-}
-
- function selectElement( options, value, name) {
-    return {
-        elementType: 'select',
-        elementConfig: {
-            options: options,
-            name: name,
-            id: name
-        },
-        value:value
-    };
-}
 
 const equipmentReducer = (state = initState, action) => {
     switch (action.type) {
@@ -60,19 +11,19 @@ const equipmentReducer = (state = initState, action) => {
             
             break;
         case "CHANGE_VALUE" :
-        //Inmutability, deep copy
-            const updatedform = { //Copy of form element
-                ...state.form
-            };
-            const updatedFormElement = { //Copy of just the updated element
-                ...updatedform[action.payload.inputIdentifier]
-            };
-            updatedFormElement.value = action.payload.value; //Update value property inside element
-            updatedform[action.payload.inputIdentifier] = updatedFormElement; //Update element changed inside form element
-            state = {//Copy state because of ID is the same
-                ...state,
-                form: updatedform
-            }
+        // //Inmutability, deep copy
+        //     const updatedform = { //Copy of form element
+        //         ...state.form
+        //     };
+        //     const updatedFormElement = { //Copy of just the updated element
+        //         ...updatedform[action.payload.inputIdentifier]
+        //     };
+        //     updatedFormElement.value = action.payload.value; //Update value property inside element
+        //     updatedform[action.payload.inputIdentifier] = updatedFormElement; //Update element changed inside form element
+        //     state = {//Copy state because of ID is the same
+        //         ...state,
+        //         form: updatedform
+        //     }
             break;
         default:
             break;
