42e2b366864e7828755d958fd896ca3fa9b54567
Josuevm <josuevm37@hotmail.com>
2017-11-26 19:22:08 -0600

    Loans working no validations

--- src/app/containers/LoanForm.js
+++ src/app/containers/LoanForm.js
@@ -2,18 +2,61 @@ import React from 'react';
 
 import "../styles/Form.css";
 import { Input, InputElement, SelectElement ,InputChangedHandler} from "../components/FormsUI/Input";
-import axios from '../'
+import axios from 'axios'
 
 class LoanForm extends React.Component {
     constructor(props) {
+        
     super();
-
+    this.selectOptions = [
+        {value:'00:00', displayValue: '00:00'},
+        {value:'01:00', displayValue: '01:00'},
+        {value:'02:00', displayValue: '02:00'},
+        {value:'03:00', displayValue: '03:00'},
+        {value:'04:00', displayValue: '04:00'},
+        {value:'05:00', displayValue: '05:00'},
+        {value:'06:00', displayValue: '06:00'},
+        {value:'07:00', displayValue: '07:00'},
+        {value:'07:30', displayValue: '07:30'},
+        {value:'08:00', displayValue: '08:00'},
+        {value:'08:30', displayValue: '08:30'},
+        {value:'09:00', displayValue: '09:00'},
+        {value:'09:30', displayValue: '9:30'},
+        {value:'10:00', displayValue: '10:00'},
+        {value:'10:30', displayValue: '10:30'},
+        {value:'11:00', displayValue: '11:00'},
+        {value:'11:30', displayValue: '11:30'},
+        {value:'12:00', displayValue: '12:00'},
+        {value:'12:30', displayValue: '12:30'},
+        {value:'13:00', displayValue: '13:00'},
+        {value:'13:30', displayValue: '13:30'},
+        {value:'14:00', displayValue: '14:00'},
+        {value:'14:30', displayValue: '14:30'},
+        {value:'15:00', displayValue: '15:00'},
+        {value:'15:30', displayValue: '15:30'},
+        {value:'16:00', displayValue: '16:00'},
+        {value:'16:30', displayValue: '16:30'},
+        {value:'17:00', displayValue: '17:00'},
+        {value:'17:30', displayValue: '17:30'},
+        {value:'18:00', displayValue: '18:00'},
+        {value:'18:30', displayValue: '18:30'},
+        {value:'19:00', displayValue: '19:00'},
+        {value:'19:30', displayValue: '19:30'},
+        {value:'20:00', displayValue: '20:00'},
+        {value:'20:30', displayValue: '20:30'},
+        {value:'21:00', displayValue: '21:00'},
+        {value:'21:30', displayValue: '21:30'},
+        {value:'22:00', displayValue: '22:00'},
+        {value:'23:00', displayValue: '23:00'},
+        {value:'24:00', displayValue: '24:00'},
+    ]
         this.state =
             {
                 form: {
-                    barcode: InputElement('text', 'Barcode', '', "barcode","barcode"),
-                    studentLicense: InputElement('text', 'Carnet', '', 'studentLicense', 'Carnet'),
-                    endDate: InputElement('date','fecha', '', "endDate",'Fecha Devolucion'),
+                    barcode: InputElement('text', 'Barcode', '', "barcode","Codigo de Barras"),
+                    peopleLicenseOrId: InputElement('text', 'Carnet/Cedula', '', 'peopleLicenseOrId', 'Carnet'),
+                    endDate: SelectElement(this.selectOptions, 
+                        '', "finishDate",'Fecha Devolucion'),
                 }
 
             };
@@ -21,12 +64,14 @@ class LoanForm extends React.Component {
 
     }
 
+
     onSubmitHandler = (event) => {
         event.preventDefault();
         const formData = {};
         for (let formElementIdentifier in this.state.form) {
             formData[formElementIdentifier] = this.state.form[formElementIdentifier].value;
         }
+
         switch (this.props.function) {
             case 'CREATE':
                 axios.post('http://localhost:8080/loans/loan', formData)
@@ -35,11 +80,17 @@ class LoanForm extends React.Component {
                     });
             break;
             case 'EDIT':
-                axios.put('http://localhost:8080/loans/loan/'+ formData)
+                axios.put('http://localhost:8080/loans/renew/', formData)
                     .then(response => {
-                        alert('prestamo Actualizado' + response);
+                        alert('prestamo Renovado' + response);
                     });
             break;
+            case 'RETURN':
+            axios.put('http://localhost:8080/loans/return/', formData)
+                .then(response => {
+                    alert('prestamo Devuelto' + response);
+                });
+        break;
         }
     }
 
@@ -47,26 +98,34 @@ class LoanForm extends React.Component {
         switch(this.props.function){
             case "CREATE":
                 this.setState({form: {
-                    barcode: InputElement('text', 'Barcode', '', "barcode","barcode"),
-                    studentLicense: InputElement('text', 'Carnet', '', 'studentLicense', 'Carnet'),
+                    barcode: InputElement('text', 'Barcode', '', "barcode","Codigo de Barras"),
+                    peopleLicenseOrId: InputElement('text', 'Carnet/Cedula', '', 'peopleLicenseOrId', 'Carnet'),
                     endDate: InputElement('date','fecha', '', "endDate",'Fecha Devolucion'),
                 }});
             break;
             case "EDIT":
                 this.setState({form: {
-                    barcode: InputElement('text', 'Barcode', '', "barcode","barcode"),
-                    endDate: InputElement('date','fecha', '', "endDate",'Fecha Devolucion'),
+                    barcode: InputElement('text', 'Barcode', '', "barcode","Codigo de Barras"),
+                    endDate: SelectElement(this.selectOptions, 
+                        '', "finishDate",'Fecha Devolucion'),
                 }});
             break;
             case "RETURN":
                 this.setState({form: {
-                    barcode: InputElement('text', 'Barcode', '', "barcode","barcode")
+                    barcode: InputElement('text', 'Barcode', '', "barcode","Codigo de Barras")
                 }});
             break;
         }
     }
 
+    componentDidUpdate(nextProps){
+        if(!(this.props.function == nextProps.function)){
+            this.formTypeHandler();
+        }
+    }
+
     render() {
+    
         const formElementsArray = [];
         for (let key in this.state.form) { //Creates an array to loop through an object attributes
             formElementsArray.push({
@@ -79,8 +138,7 @@ class LoanForm extends React.Component {
         return (
             <div className="formSpace">
                 <div className="row">
-                    {/* <form> falta onSubmit */}
-                    <form>
+                    <form onSubmit={(event) => this.onSubmitHandler(event)}>
                         <div className="col-sm-2">
                             {formElementsArray.map(formElement => (
                                 <div>
